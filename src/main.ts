b1bed16b68426be56cea22ee4f00e3bf2c3ef21f
Sarunya <sarunya46mk@gmail.com>
2026-03-21 13:04:55 +0700

    chore: init project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port = process.env.API_PORT ?? 4000;
+  const logger = new Logger('Bootstrap');
+  await app.listen(port);
+  logger.log(`Running on http://localhost:${port}/api`);
+  logger.log(`Swagger at http://localhost:${port}/api-docs`);
+}
+void bootstrap();
