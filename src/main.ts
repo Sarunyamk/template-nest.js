67b72fbfaf07351694a46b1ebe5b164440701f7f
Sarunya <sarunya46mk@gmail.com>
2026-03-21 15:09:59 +0700

    feat(CORS): add cors config in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,15 @@ import { GlobalValidationPipe } from './common/pipes/global-validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS?.split(',') ?? [
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(new GlobalValidationPipe());
 
   const port = process.env.API_PORT ?? 4000;
